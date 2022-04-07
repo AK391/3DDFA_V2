80bcd61bd54542f7ade0ceb8721b6b1ea5cb3639
AK391 <81195143+AK391@users.noreply.github.com>
2022-04-07 13:48:18 -0400

    Update nms_wrapper.py

--- FaceBoxes/utils/nms_wrapper.py
+++ FaceBoxes/utils/nms_wrapper.py
@@ -7,13 +7,22 @@
 # Written by Ross Girshick
 # --------------------------------------------------------
 
-from .nms.cpu_nms import cpu_nms, cpu_soft_nms
+#from .nms.cpu_nms import cpu_nms, cpu_soft_nms
+from nms.py_cpu_nms import py_cpu_nms
 
 
+# def nms(dets, thresh):
+#     """Dispatch to either CPU or GPU NMS implementations."""
+
+#     if dets.shape[0] == 0:
+#         return []
+#     return cpu_nms(dets, thresh)
+#     # return gpu_nms(dets, thresh)
+    
 def nms(dets, thresh):
     """Dispatch to either CPU or GPU NMS implementations."""
 
     if dets.shape[0] == 0:
         return []
-    return cpu_nms(dets, thresh)
-    # return gpu_nms(dets, thresh)
+    else:
+        return py_cpu_nms(dets, thresh)
